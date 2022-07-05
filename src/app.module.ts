267dacb40d3fd49844ddf819460dd78b28eb6658
Nathan Garcia dos Santos <nathangar01@gmail.com>
2022-07-05 20:13:57 -0300

    Adding file in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SpecieModule } from './specie/specie.module';
+import { FileController } from './file/file.controller';
 
 @Module({
   imports: [SpecieModule],
+  controllers: [FileController],
 })
 export class AppModule {}
