7a8d0ee1f11fa06357967b7a50fa03c23e63c0ac
Nathan Santos <nathan.santos@finnet.com.br>
2023-03-03 15:03:42 -0300

    feat: adding helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from "helmet"
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet())
+  app.use(helmet());
   app.setGlobalPrefix('api/v1/');
   await app.listen(8080);
 }
